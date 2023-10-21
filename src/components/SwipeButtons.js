95733eb14d53713bbbf07f196178f23ff30b0d7f
nicolinif <fnicolini16@outlook.com>
2023-10-21 01:36:04 -0300

    se agregaron nuevos estilos a las cartas. Se emprolijo el codigo respecto a identacion.

--- src/components/SwipeButtons.js
+++ src/components/SwipeButtons.js
@@ -5,21 +5,15 @@ import CloseIcon from "@mui/icons-material/Close";
 import StarRateIcon from "@mui/icons-material/StarRate";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 
-
 import FlashOnIcon from "@mui/icons-material/FlashOn";
 import IconButton from '@mui/material/IconButton';
 
 const SwipeButtons = () => {
-  const handleReplayClick = () => {
-    // Realiza la acción deseada cuando se hace clic en el botón "Replay".
-    // Esto podría ser deshacer una acción anterior o realizar alguna otra tarea.
-    console.log("Botón Replay clicado");
-    // Realiza aquí la acción que necesitas
-  };
+  
 
   return (
     <div className="swipeButtons">
-      <IconButton  onClick={handleReplayClick} className="swipeButtons__repeat">
+      <IconButton  className="swipeButtons__repeat">
         <ReplayIcon fontSize="large" />
       </IconButton>
       <IconButton className="swipeButtons__left">
--- src/components/TinderCards.js
+++ src/components/TinderCards.js
@@ -8,52 +8,50 @@ import { wait } from "@testing-library/user-event/dist/utils";
 function TinderCards() {
   const [people, setPeople] = useState([
     { name: "Paylord Swift", 
-      url : "https://imagenes.elpais.com/resizer/JRf0j6Vdr_klRRV4E5Ze3a41HKs=/414x0/cloudfront-eu-central-1.images.arcpublishing.com/prisa/SC366MQEQNDVRHXSDEQMKHM5DA.jpg"
+      url : "https://imagenes.elpais.com/resizer/JRf0j6Vdr_klRRV4E5Ze3a41HKs=/414x0/cloudfront-eu-central-1.images.arcpublishing.com/prisa/SC366MQEQNDVRHXSDEQMKHM5DA.jpg",
+      age: "25",
+      distancia: "2 km"
     },
     { name: "Taylor Swift", 
-      url : "https://cdn2.excelsior.com.mx/media/styles/image800x600/public/pictures/2023/05/10/2947481.jpg"
+      url : "https://cdn2.excelsior.com.mx/media/styles/image800x600/public/pictures/2023/05/10/2947481.jpg",
+      age: "19",
+      distancia: "1 km"
     },
-
-
-
   ]);
 
-
-  const onSwipe = (index) => {
-    //Make this function sleep for 1 sec
-    
-
-
+  const onSwipe = (index) => {     //Make this function sleep for 1 sec
     setPeople((prevPeople) => prevPeople.filter((person, i) => i !== index))
-
     }
-
 
   return (
     <div>
     <Header></Header>
       <div className="tinderCards__cardContainer">
         {people.map((person) => (
-          <TinderCard onSwipe={() => onSwipe(people.indexOf(person))}
+          <TinderCard        
             className="swipe"
             key={person.name}
             preventSwipe={["up", "down"]}
             swipeRequirementType="position"
             swipeThreshold={280}
+             /*onCardLeftScreen - posiblemente sirva para poner invisible la card una vez swipeada*/
           >
             <div
               style={{ backgroundImage: `url(${person.url})` }}
               className="card"
             >
-              <h3>{person.name}</h3>
+              <h2>{person.name}</h2>
+              <h3 className="edad">{person.age}</h3>
+              <h3 className="distancia">{person.distancia}</h3>
             </div>
           </TinderCard>
         ))}
       </div>
-      <SwipeButtons />
+      <SwipeButtons>   
+      </SwipeButtons>
     </div>
-    
   );
-
   }
 export default TinderCards;
+
+
